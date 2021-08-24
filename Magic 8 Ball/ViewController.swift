1c33dc64f88a9873249da4cae62d03f57c6cebb5
Marymezh <maria.mezhova@yahoo.com>
2021-08-24 16:02:04 +0700

    IB Outlets were created for the project

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+
+    @IBOutlet weak var imageView: UIImageView!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
+    
+  
+    @IBAction func AskButtonPressed(_ sender: UIButton) {
+        
+        imageView.image = ballArray[2]
+    }
 
 }
 
